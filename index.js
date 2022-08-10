07ef2ae459e4b2d5c829fe8feecace0a2d9bf060
githubarj <githuba@GitthubaRJ.localdomain>
2022-08-10 12:01:13 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const root = document.getElementById("root")
+const information = document.getElementById("information")
+
+ReactDOM.render(
+  <div>
+    <h1>Hello, everyone</h1>
+    <p>I am Daniel Kiptoo</p>
+  </div>,
+  root
+);
+// what we want to render and where we want to render
+// ReactDOM.render(<p>I am Richard Jeremy</p>, information)
